c3f5b75f202be9b17c8b85c05dd3ab224d13cfe1
Bryang2303 <bryang2303@gmail.com>
2020-09-20 13:42:28 -0600

    Initial commit

---
+++ src/Tarea.java
@@ -0,0 +1,19 @@
+import javafx.application.Application;
+import javafx.scene.Cursor;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.scene.control.Button;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.StageStyle;
+
+import javax.swing.*;
+
+public class Tarea extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
